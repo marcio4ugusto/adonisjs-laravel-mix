a71edd51f0d177a329fa0a9cc5a22d12b7167411
Marcio Costa <marcim.bernardes@gmail.com>
2018-06-06 19:54:24 -0300

    Laravel Mix updates

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,15 @@
 let mix = require('laravel-mix');
 
 mix.setPublicPath('public');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application, as well as bundling up your JS files.
+ | for your Adonis application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
  |
  */
 
